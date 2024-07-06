2fc64887183adbb6b1a50e6fccb17eafd2b3b60f
Jason Mulligan <jason.mulligan@avoidwork.com>
2024-07-06 14:00:49 -0400

    Fixing test descriptions

--- test/jsonl.js
+++ test/jsonl.js
@@ -2,13 +2,13 @@ import assert from "node:assert";
 import {jsonl} from "../dist/tiny-jsonl.cjs";
 
 describe("Testing functionality", function () {
-	it("It should jsonl an Object", function () {
+	it("It should handle an Object", function () {
 		const jsonldObject = jsonl({"id": "test-123", "email": "test@example.com"});
 		const expectedResult = "{\"id\": \"test-123\", \"email\": \"test@example.com\"}";
 		assert.strictEqual(jsonldObject, expectedResult, "Should match the jsonld string");
 	});
 
-	it("It should jsonl an Array", function () {
+	it("It should handle an Array of Objects", function () {
 		const jsonldObject = jsonl([{"id": "test-123", "email": "test@example.com"}, {"id": "test-456", "email": "test2@example.com"}]);
 		const expectedResult = "{\"id\": \"test-123\", \"email\": \"test@example.com\"}\n{\"id\": \"test-456\", \"email\": \"test2@example.com\"}";
 		assert.strictEqual(jsonldObject, expectedResult, "Should match the jsonld string");
