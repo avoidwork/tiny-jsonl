b5199a26bcbfe47d153063d578043e67f453b039
Jason Mulligan <jason.mulligan@avoidwork.com>
2024-07-06 17:09:57 -0400

    Updating test typos

--- test/jsonl.js
+++ test/jsonl.js
@@ -3,15 +3,15 @@ import {jsonl} from "../dist/tiny-jsonl.cjs";
 
 describe("Testing functionality", function () {
 	it("It should handle an Object", function () {
-		const jsonldObject = jsonl({"id": "test-123", "email": "test@example.com", "field1:field2": "d,e", "description": "this field has \"embed quotes\"", "object": {"abc": true, "def": false}, "array": ["ghi", 1234, "other string", {"nested": true}]});
+		const jsonlObject = jsonl({"id": "test-123", "email": "test@example.com", "field1:field2": "d,e", "description": "this field has \"embed quotes\"", "object": {"abc": true, "def": false}, "array": ["ghi", 1234, "other string", {"nested": true}]});
 		const expectedResult = "{\"id\": \"test-123\", \"email\": \"test@example.com\", \"field1:field2\": \"d,e\", \"description\": \"this field has \\\"embed quotes\\\"\", \"object\": {\"abc\": true, \"def\": false}, \"array\": [\"ghi\", 1234, \"other string\", {\"nested\": true}]}";
-		strictEqual(jsonldObject, expectedResult, "Should match the jsonld string");
+		strictEqual(jsonlObject, expectedResult, "Should match the jsonl string");
 	});
 
 	it("It should handle an Array of Objects", function () {
-		const jsonldObject = jsonl([{"id": "test-123", "email": "test@example.com"}, {"id": "test-456", "email": "test2@example.com"}]);
+		const jsonlObject = jsonl([{"id": "test-123", "email": "test@example.com"}, {"id": "test-456", "email": "test2@example.com"}]);
 		const expectedResult = "{\"id\": \"test-123\", \"email\": \"test@example.com\"}\n{\"id\": \"test-456\", \"email\": \"test2@example.com\"}";
-		strictEqual(jsonldObject, expectedResult, "Should match the jsonld string");
+		strictEqual(jsonlObject, expectedResult, "Should match the jsonl string");
 	});
 
 	it("It should throw a TypeError if receiving a String", function () {
